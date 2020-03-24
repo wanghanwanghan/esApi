bec6c55e8975e1062640159dbd0ee26d5a961e4a
wanghan <minglongoc@me.com>
2020-03-24 18:18:42 +0800

    321

--- App/HttpController/Business/Test/TestController.php
+++ App/HttpController/Business/Test/TestController.php
@@ -22,18 +22,40 @@ class TestController extends BusinessBase
     public function index()
     {
         $url='https://api.fahaicc.com/v2/query/sat?';
-        $data=[
-            'doc_type' => 'satparty_qs',
-            'keyword' => '小米',
-            'pageno' => 1,
+
+        $rt=time()*1000;
+
+        $sign_num=md5('25DwLghyWYbHM9P0OjwW',$rt);
+
+        $json_data=[
+            'dataType'=>'satparty_qs',
+            'keyword'=> '小米',
+            'pageno'=>1,
             'range'=>10
         ];
-        $res=Fahai::getInstance()->sendPost($url,$data);
+
+        $data=[
+            'authCode'=>'25DwLghyWYbHM9P0OjwW',
+            'rt'=>$rt,
+            'sign'=>$sign_num,
+            'args'=>json_encode($json_data)
+        ];
 
 
-        var_dump($res);
 
-        $this->writeJson(200,$res,'suc');
+        $curl=curl_init();
+        curl_setopt($curl,CURLOPT_URL,$url);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($curl,CURLOPT_POST, 1);
+        curl_setopt($curl,CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);
+        $output=curl_exec($curl);
+        curl_close($curl);
+
+
+
+        $this->writeJson(200,json_decode($output,1),'suc');
 
 
         return true;
--- App/HttpController/Server/DataSource/Fahai.php
+++ App/HttpController/Server/DataSource/Fahai.php
@@ -10,36 +10,5 @@ class Fahai extends ServerBase
 {
     use Singleton;
 
-    private $authCode='25DwLghyWYbHM9P0OjwW';
-
-    //发送请求
-    public function sendPost($url,$body)
-    {
-        $rt=time()*1000;
-
-        $sign_num=md5($this->authCode,$rt);
-
-        $doc_type=$body['doc_type'];
-        $keyword=$body['keyword'];
-        $pageno=$body['pageno'];
-        $range=$body['range'];
-
-        $json_data=[
-            'dataType'=>$doc_type,
-            'keyword'=> $keyword,
-            'pageno'=>$pageno,
-            'range'=>$range
-        ];
-
-        $data=[
-            'authCode'=>$this->authCode,
-            'rt'=>$rt,
-            'sign'=>$sign_num,
-            'args'=>$json_data
-        ];
-
-        return $this->curl($url,$data);
-    }
-
 
 }
