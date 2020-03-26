cf30d4a0128260bb1df05d7f87b50be612674238
wanghan <minglongoc@me.com>
2020-03-26 12:48:47 +0800

    321

--- vendor/easyswoole/easyswoole/src/Logger.php
+++ vendor/easyswoole/easyswoole/src/Logger.php
@@ -31,8 +31,8 @@ class Logger implements LoggerInterface
     {
         $str = $this->logger->log($msg,$logLevel,$category);
         $calls = $this->callback->all();
+        var_dump($calls);
         foreach ($calls as $call){
-            var_dump($call,$msg,$logLevel,$category);
             call_user_func($call,$msg,$logLevel,$category);
         }
         return $str;
